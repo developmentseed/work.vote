e093ed3542436fbabc5c9bb92c8ec3abd3b0a174
Scisco <scisco7@gmail.com>
2015-12-21 03:35:37 -0500

    use react components

--- app/assets/scripts/components/frontpage.js
+++ app/assets/scripts/components/frontpage.js
@@ -1,24 +1,18 @@
-let React = require('react');
-import { Link } from 'react-router';
+import React from 'react';
+import Map from './map'
+import Search from './search';
 
 let Frontpage = React.createClass({
 	render: function () {
 		return (
 			<div className="row column">
 				<div className="banner-image">
-				<img src='/assets/graphics/layout/main.jpg' width='100%' />
+					<img src='/assets/graphics/layout/main.jpg' width='100%' />
 				</div>
 				<div className="map-break"></div>
 				<div id='User-Locate-container'>
-					<div id='Search-container'>
-						<div id="Address-Finder">
-							<div className="center-text"></div>
-	 						<input type="text" placeholder="find your jursidiction" />
-	 						<div className="usemap">Locate via map</div>
-						</div>
-					</div>
-					<div className="User-Locate-return"><img src="/assets/graphics/layout/search.png"></img> search</div>
-					<div id='map'></div>
+					<Search />
+					<Map />
 				</div>
 			</div>
 		);
