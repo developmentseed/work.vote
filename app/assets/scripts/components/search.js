3960bc31f29afce5913c4395209424062adcb60b
Scisco <scisco7@gmail.com>
2016-01-06 18:48:31 -0500

    linting search

--- app/assets/scripts/components/search.js
+++ app/assets/scripts/components/search.js
@@ -1,16 +1,17 @@
-import _ from 'lodash';
+'use strict';
+
 import geo from 'mapbox-geocoding';
 import React from 'react';
-import config from '../config'
-import store from '../store'
+import config from '../config';
+import store from '../store';
 import nets from 'nets';
-import { pushPath } from 'redux-simple-router'
+import { pushPath } from 'redux-simple-router';
 import Autosuggest from 'react-autosuggest';
 
-let juris = {}
+let juris = {};
 
 let Search = React.createClass({
-
+  displayName: 'Search',
   getSuggestions: function (input, callback) {
     let options = new Set();
     geo.setAccessToken(config.accessToken);
@@ -18,34 +19,35 @@ let Search = React.createClass({
       method: 'get',
       encoding: undefined,
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       },
       url: `${config.apiUrl}/jurisdictions/?search=${input}`
-    }, function(err, resp, body) {
+    }, function (err, resp, body) {
+      console.log(err);
       let results = JSON.parse(body).results;
       for (let j = 0; j < results.length; j++) {
         let option = results[j].name + ', ' + results[j].state.alpha;
         juris[option] = results[j].id;
-        options.add(option)
+        options.add(option);
       }
       callback(null, Array.from(options));
     });
   },
 
   onSuggestionSelected: function (value, event) {
-    store.dispatch(pushPath(`jurisdictions/${juris[value]}`))
+    store.dispatch(pushPath(`jurisdictions/${juris[value]}`));
   },
 
   render: function () {
     return (
       <div>
         <div id='Search-container'>
-          <div id="Address-Finder">
-            <div className="center-text">Enter your county, zipcode, or address</div>
+          <div id='Address-Finder'>
+            <div className='center-text'>Enter your county, zipcode, or address</div>
             <Autosuggest suggestions={this.getSuggestions} onSuggestionSelected={this.onSuggestionSelected}/>
           </div>
         </div>
-        <div id="Search-enabler"><img src="/assets/graphics/layout/search.png"></img></div>
+        <div id='Search-enabler'><img src='/assets/graphics/layout/search.png'></img></div>
       </div>
     );
   }
