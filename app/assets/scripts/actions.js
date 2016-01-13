63976ef19086317fceabfd5f68fd33605555e7d0
Scisco <scisco7@gmail.com>
2016-01-13 00:50:35 -0500

    new action store for reseting jurisdiction

--- app/assets/scripts/actions.js
+++ app/assets/scripts/actions.js
@@ -4,6 +4,10 @@ module.exports.fetchJurisdiction = function fetchJurisdiction (jurisdiction) {
   return { type: 'FETCH_JURISDICTION', data: jurisdiction };
 };
 
+module.exports.resetJurisdiction = function resetJurisdiction () {
+  return { type: 'RESET_JURISDICTION' };
+};
+
 module.exports.stateChangeToFalse = function () {
   return { type: 'CANCEL', data: false };
 };
