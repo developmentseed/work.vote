cf1ede9509d12dac7a6a5180b99988ad90a86ea8
Scisco <scisco7@gmail.com>
2016-01-20 10:50:58 -0500

    go back to previous contacts

--- app/assets/scripts/views/contact.js
+++ app/assets/scripts/views/contact.js
@@ -6,18 +6,16 @@ import Box from '../components/box';
 let Contact = React.createClass({
   render: function () {
     return (
-      <Box>
-        <div className='results-split-container'>
-          <div className = 'text-header'>Contact Us *</div>
-          <p>First and last name please</p>
-          <div className = 'text-header'>Email Addresss *</div>
-          <p>Your Answer</p>
-          <div className = 'text-header'>Comment or Questions *</div>
-          <p>Please leave your comment or question here</p>
-          <div className = 'btn'>Submit</div>
+      <div className='large'>
+        <div id='results-container'>
+          <div className='columns medium-centered'>
+            <br />
+              <iframe src='https://docs.google.com/forms/d/1Q5uB-Tsue4ArwEqxDYDISN6tgldhB-D_2QljGIQgfhs/viewform?embedded=true#start=embed' width='860' height='900' frameBorder='0' marginHeight='0' marginWidth='0'>Loading...</iframe>
+
+          </div>
         </div>
-      </Box>
-		);
+      </div>
+    );
   }
 });
 
