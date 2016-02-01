005a13ae588e8acd5c08d948ebd44b936da45a65
Lauren Jacobson <lauren@developmentseed.org>
2016-02-01 15:37:20 -0500

    fixed closing tag

--- app/assets/scripts/views/contact.js
+++ app/assets/scripts/views/contact.js
@@ -8,10 +8,10 @@ let Contact = React.createClass({
     return (
       <Box>
         <div className='results-split-container'>
-          <div className = 'contact-questions'>Contact Us *</div>
-          <input class="form__control" type="text" placeholder="Placeholder" id="control-1">
+          <div className = 'contact-questions'>Contact Us </div>
+          <input class="form__control" type="text" placeholder="Placeholder" id="control-1" />
           <div className = 'contact-input'><p>Your Answer</p></div>
-          <div className = 'contact-questions'>Comment or Questions *</div>
+          <div className = 'contact-questions'>Comment or Questions</div>
           <div className = 'contact-input'><p>Please leave your comment or question here</p></div>
           <p><div className = 'btn'>Submit</div></p>
         </div>
