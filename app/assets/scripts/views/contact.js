94a3bc692d7e471e653c8bfa1ea515fa732a55df
Lauren Jacobson <lauren@developmentseed.org>
2016-02-01 17:01:56 -0500

    Contact page updates

--- app/assets/scripts/views/contact.js
+++ app/assets/scripts/views/contact.js
@@ -8,12 +8,23 @@ let Contact = React.createClass({
     return (
       <Box>
         <div className='results-split-container'>
-          <div className = 'contact-questions'>Contact Us </div>
-          <input class="form__control" type="text" placeholder="Placeholder" id="control-1" />
-          <div className = 'contact-input'><p>Your Answer</p></div>
-          <div className = 'contact-questions'>Comment or Questions</div>
-          <div className = 'contact-input'><p>Please leave your comment or question here</p></div>
-          <p><div className = 'btn'>Submit</div></p>
+            <div className = 'large-12 columns text-header'>Contact Us </div>
+            <hr />
+            <div className = 'contact-questions'>Name </div>
+            <div className= 'large-6 medium-6 columns'>
+              <input class="form__control" type="text" placeholder="First and Last Name" id="control-1" />
+            </div>
+            <div className = 'large-12 columns contact-questions'>Email </div>
+            <div className= 'large-6 medium-6 columns'>
+              <input class="form__control" type="text" placeholder="Email" id="control-1" />
+            </div>
+            <div className = 'large-12 columns contact-questions'>Comment or Questions</div>
+            <div className= 'large-6 medium-6 columns'>
+             <textarea class="form__control" type="text" placeholder="Type Here" id="control-1" />
+            </div>
+            <div className = 'large-12 columns'>
+              <div className = 'btn'>Submit</div>
+            </div>
         </div>
       </Box>
 		);
