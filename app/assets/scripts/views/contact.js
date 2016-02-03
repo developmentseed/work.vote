cf49307c1fb11ea50f4414814d0fe139e29c93f0
Scisco <scisco7@gmail.com>
2016-02-03 06:20:38 -0500

    fix contact form

--- app/assets/scripts/views/contact.js
+++ app/assets/scripts/views/contact.js
@@ -1,31 +1,38 @@
 'use strict';
 
 import React from 'react';
+import Box from '../components/box';
 
 let Contact = React.createClass({
   render: function () {
     return (
       <Box>
         <div className='results-split-container'>
-            <div className = 'large-12 columns text-header'>Contact Us </div>
-            <hr />
+          <div className = 'large-12 columns text-header'>Contact Us </div>
+          <hr />
+
+          <div className= 'large-6 medium-6 columns'>
             <div className = 'contact-questions'>Name </div>
-            <div className= 'large-6 medium-6 columns'>
-              <input class="form__control" type="text" placeholder="First and Last Name" id="control-1" />
-            </div>
+            <input className='form__control' type='text' placeholder='First and Last Name' id='control-1' />
             <div className = 'large-12 columns contact-questions'>Email </div>
-            <div className= 'large-6 medium-6 columns'>
-              <input class="form__control" type="text" placeholder="Email" id="control-1" />
-            </div>
+            <input className='form__control' type='text' placeholder='Email' id='control-1' />
             <div className = 'large-12 columns contact-questions'>Comment or Questions</div>
-            <div className= 'large-6 medium-6 columns'>
-             <textarea class="form__control" type="text" placeholder="Type Here" id="control-1" />
+            <textarea className='form__control' type='text' placeholder='Type Here' id='control-1' />
+            <div className = 'btn'>Submit</div>
+          </div>
+
+          <div className= 'large-6 medium-6 columns'>
+            <div className = 'contact-address'>
+              <p>1825 K St. NW Suite 450</p>
+              <p>Washington DC 20006</p>
+              <p>(202) 331-1550</p>
+              <p>info@fairelectionsnetwork.com</p>
+              <p>www.FairElectionsNetwork.com</p>
             </div>
-            <div className = 'large-12 columns'>
-              <div className = 'btn'>Submit</div>
-            </div>
+          </div>
+
         </div>
-      </div>
+      </Box>
     );
   }
 });
