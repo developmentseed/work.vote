16831bec44fa2ba5e7dd08c8c6a3519bff6c7039
Scisco <scisco7@gmail.com>
2016-02-25 12:54:48 -0500

    shape creator func

--- app/assets/scripts/utils.js
+++ app/assets/scripts/utils.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import d3 from 'd3';
 import nets from 'nets';
 
 export function fetch (url, callback) {
@@ -24,4 +25,47 @@ export function checkUrl (url) {
   } else {
     return url;
   }
+};
+
+export function shape (el, geojson) {
+  var map = d3.select(el);
+
+  var width = parseInt(map.style('width'), 10);
+  var height = parseInt(map.style('height'), 10);
+
+  var vis = map.append('svg')
+      .attr('width', width).attr('height', height);
+
+  // create a first guess for the projection
+  var center = d3.geo.centroid(geojson);
+  var scale = 150;
+  var offset = [width / 2, height / 2];
+  var projection = d3.geo.mercator().scale(scale).center(center)
+      .translate(offset);
+
+  // create the path
+  var path = d3.geo.path().projection(projection);
+
+  // using the path determine the bounds of the current map and use
+  // these to determine better values for the scale and translation
+  var bounds = path.bounds(geojson);
+  var hscale = scale * width / (bounds[1][0] - bounds[0][0]);
+  var vscale = scale * height / (bounds[1][1] - bounds[0][1]);
+  scale = (hscale < vscale) ? hscale : vscale;
+  offset = [width - (bounds[0][0] + bounds[1][0]) / 2,
+                    height - (bounds[0][1] + bounds[1][1]) / 2];
+
+  // new projection
+  projection = d3.geo.mercator().center(center)
+    .scale(scale).translate(offset);
+  path = path.projection(projection);
+
+  // add a rectangle to see the bound of the svg
+  vis.append('rect').attr('width', width).attr('height', height)
+    .style('fill', 'none');
+
+  vis.selectAll('path').data([geojson]).enter().append('path')
+    .attr('d', path)
+    .style('fill', '#a5a5a5')
+    .style('stroke-width', '1');
 };
