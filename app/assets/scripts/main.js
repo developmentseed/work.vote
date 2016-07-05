cc30c73d10bcda45142f1407c35be339543ee2d7
Scisco <scisco7@gmail.com>
2016-07-05 12:04:42 -0400

    fixes #64

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -9,6 +9,7 @@ import { Router, Route, IndexRoute, useRouterHistory} from 'react-router';
 import { createHashHistory } from 'history';
 import { syncHistory } from 'react-router-redux';
 import reducer from './reducers/reducer';
+import ReactGA from 'react-ga';
 
 // Views
 import App from './views/app';
@@ -18,6 +19,14 @@ import States from './views/states';
 import Frontpage from './views/frontpage';
 import Contact from './views/contact';
 import Empty from './views/404';
+
+// Google analytics
+ReactGA.initialize('UA-65976828-4');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname + window.location.search  + window.location.hash });
+  ReactGA.pageview(window.location.pathname + window.location.search  + window.location.hash);
+}
 
 const history = useRouterHistory(createHashHistory)({ queryKey: false });
 const reduxRouterMiddleware = syncHistory(history);
@@ -30,13 +39,13 @@ const store = createStoreWithMiddleware(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
+    <Router history={history} onUpdate={logPageView}>
       <Route path='/' component={App}>
         <Route path='j(/:jurisdiction_id)' component={Result} />
         <Route path='p(/:slug)' component={Page} />
         <Route path='contact' component={Contact} />
         <Route path='states(/:state_id)' component={States} />
-        <Route path='*' component={Empty}/>
+        <Route path='*' component={Empty} />
         <IndexRoute component={Frontpage} />
       </Route>
 
